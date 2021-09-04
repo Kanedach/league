c5d274f9ff49899689fa59246981c2ba1616061c
Stutz Fernando, INI-DOS-DCI-S2A <Fernando.Stutz@swisscom.com>
2021-09-04 13:32:17 +0200

    Init

--- apps/league/src/app/app.module.ts
+++ apps/league/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, StoreModule.forRoot({}, {}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }), EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
